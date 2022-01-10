83bb8dd6f7008672bcde46f9637e2cc78edcd6de
itz-deepanshu <97439677+itz-deepanshu@users.noreply.github.com>
2022-01-10 16:37:50 +0530

    Add files via upload

---
+++ assign1_outputfile_CSE_21103078.py
@@ -0,0 +1,40 @@
+"C:\Program Files\Python310\python.exe" D:/Python1/assignment/assign1_CSE_21103078.py
+Question 1
+Enter first number:7
+Enter second number:6
+Enter third number:3
+The average of three numbers is :
+5.333333333333333
+
+Question 2
+enter your gross income:100000
+Enter your number of dependents:4
+Your income tax is :
+15600.0
+
+
+Question 3
+Enter your SID:21103078
+Enter your name:Deepanshu
+Enter your Gender: Male
+Enter your course name:CSE
+Enter your CGPA:10
+['21103078', 'Deepanshu', 'Male', 'CSE', 10.0]
+ 
+Question 4
+
+
+Enter marks of students:99
+Enter marks of students:98
+Enter marks of students:87
+Enter marks of students:100
+Enter marks of students:78
+['100', '78', '87', '98', '99']
+
+
+Question 5
+
+Part a question :  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b of question : ['Red', 'Green', 'White', 'Purple', 'Yellow']
+
+Process finished with exit code 0
