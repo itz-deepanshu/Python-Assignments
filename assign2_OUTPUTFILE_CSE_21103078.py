99ad7f5e1f9e149afe2f8e5aea193bf893cbedf6
itz-deepanshu <97439677+itz-deepanshu@users.noreply.github.com>
2022-01-22 01:42:24 +0530

    Add files via upload

---
+++ assign2_OUTPUTFILE_CSE_21103078.py
@@ -0,0 +1,45 @@
+"C:\Program Files\Python310\python.exe" D:/Python1/assignment/assign2_CSE_21103078.py
+
+
+#QUESTION 1
+<A>THE LENGTH OF STRING IS 35
+<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP
+<E>THE INDEX OF SUBSTRING a is  10
+<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage
+
+#QUESTION 2
+ENTER YOUR NAMEDeepanshu
+ENTER YOUR SID21103078
+ENTER YOUR DEPARTMENTCSE
+ENTER YOUR CGPA10
+Hey Deepanshu, Here!
+My SID is 21103078
+I am from CSE and my CGPA is 10.000000
+
+#QUESTION 3
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+#QUESTION 4
+Enter first number: 23
+Enter second number: 43
+Enter third number: 29
+THE LARGEST NUMBER IS  43
+
+#QUESTION 5
+ENTER A STRINGDeepanshu
+No
+
+
+#QUESTION 6
+ENTER FIRST SIDE OF TRIANGLE34
+ENTER SECOND SIDE OF TRIANGLE54
+ENTER THIRD SIDE OF TRIANGLE84
+Yes
+
+
+
+Process finished with exit code 0
