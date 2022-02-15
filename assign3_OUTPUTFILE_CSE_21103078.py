bcbc13d5410bc9601f86477337bb3441ec0e410f
itz-deepanshu <97439677+itz-deepanshu@users.noreply.github.com>
2022-02-15 13:28:45 +0530

    Add files via upload

---
+++ assign3_OUTPUTFILE_CSE_21103078.py
@@ -0,0 +1,86 @@
+"C:\Program Files\Python310\python.exe" D:/Python1/assignment/assign3_CSE_21103078.py
+QUESTION 1
+
+ENTER ANY STRING: deepanshu
+{'d': 1, 'e': 2, 'p': 1, 'a': 1, 'n': 1, 's': 1, 'h': 1, 'u': 1}
+
+
+
+QUESTION 2
+
+ENTER THE DAY: 24
+ENTER THE MONTH OF THE YEAR: 12
+ENTER THE YEAR: 2024
+25 / 12 / 2024
+
+
+QUESTION 3
+
+Enter elements of a list separated by space 4 8 13
+list:  4 8 13
+[(4, 16), (8, 64), (13, 169)]
+
+
+QUESTION 4
+
+Enter Grade Point: 9
+Your Grade is 'A' and Excellent performance
+
+
+QUESTION 5
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+QUESTION 6
+
+Enter SID: 21103078
+Enter Name: Deepanshu
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103112
+Enter Name: Aman
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103001
+Enter Name: Shrey
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103021
+Enter Name: Dhruv
+Do you want to enter another student entry(Y or N):N
+<a> {21103078: 'Deepanshu', 21103112: 'Aman', 21103001: 'Shrey', 21103021: 'Dhruv'}
+<b> {21103112: 'Aman', 21103078: 'Deepanshu', 21103021: 'Dhruv', 21103001: 'Shrey'}
+<c> {21103001: 'Shrey', 21103021: 'Dhruv', 21103078: 'Deepanshu', 21103112: 'Aman'}
+Search Name with SID: 21103078
+<d> Deepanshu
+
+
+QUESTION 7
+
+ENTER THE NUMBER OF TERMS IN THE SERIES: 9
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 6.0
+
+
+
+QUESTION 8
+
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
+
+Process finished with exit code 0
