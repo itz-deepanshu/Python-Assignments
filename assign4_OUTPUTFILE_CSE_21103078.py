cbc126ed00b2ff1c90d20fa3afd31d2133fa180d
itz-deepanshu <97439677+itz-deepanshu@users.noreply.github.com>
2022-02-26 21:51:41 +0530

    Add files via upload

---
+++ assign4_OUTPUTFILE_CSE_21103078.py
@@ -0,0 +1,59 @@
+"C:\Program Files\Python310\python.exe" D:/Python1/assignment/assign4_CSE_21103078.py
+ANSWER 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+ANSWER 2
+Enter the size of pascals triangle 5
+USING LOOPS
+      1
+     1 1
+    1 2 1
+   1 3 3 1
+  1 4 6 4 1
+
+
+
+USING RECURSSIONS
+        1
+      1   1
+    1   2   1
+  1   3   3   1
+1   4   6   4   1
+
+
+ANSWER 3
+Enter 2 numbers: 10 4
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> All the values are non zero
+<c> Filtered out numbers that are greater than 4 are : [6, 6, 7, 7, 7, 8, 8]
+<d> Set: {8, 6, 7}
+<e> Immutable set: frozenset({8, 6, 7})
+<f> Hash value of the max value from the above set: 8
+
+
+ANSWER 4
+Object created
+The name of the student it Mishank and SID is 21103107.
+Object destroyed
+
+
+ANSWER 5
+<a> The updated salary of Mehak is 70000
+<b>Record of Viren deleted
+
+ANSWER 6
+Enter the word: deal
+Enter a new meaningful word using the exact same letters to test your friendship: lead
+Does the word makes sense?(y/Y or n/N )Y
+The friends pass the friendship test
+
+Process finished with exit code 0
